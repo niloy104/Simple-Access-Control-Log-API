45d13291c94c66bd59df8ab75349b15ced3eb2f8
niloy104 <manzurul104@gmail.com>
2026-01-06 16:01:35 +0600

    implement models.py file

--- access_control/models.py
+++ access_control/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class AccessLog(models.Model):
+    card_id = models.CharField(max_length=20)
+    door_name = models.CharField(max_length=50)
+    access_granted = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.card_id} - {self.door_name} - {'GRANTED' if self.access_granted else 'DENIED'}"
