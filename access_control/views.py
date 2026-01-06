b8dee1791492f0d275f59974d523a8f840a55d7d
niloy104 <manzurul104@gmail.com>
2026-01-06 16:05:37 +0600

    implement drf serializers and curd views

--- access_control/views.py
+++ access_control/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import AccessLog
+from .serializers import AccessLogSerializer
 
-# Create your views here.
+class AccessLogListCreate(generics.ListCreateAPIView):
+    queryset = AccessLog.objects.all()
+    serializer_class = AccessLogSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        card_id = self.request.GET.get('card_id')
+        if card_id:
+            queryset = queryset.filter(card_id=card_id)
+        return queryset
+
+class AccessLogRetrieveUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = AccessLog.objects.all()
+    serializer_class = AccessLogSerializer
